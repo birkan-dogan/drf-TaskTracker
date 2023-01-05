93c6644b163b2fcbe7f59da7b7a6753852d3d300
birkan <b.561birkan@gmail.com>
2023-01-05 21:14:50 +0300

    endpoints are ready to take requests

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from todo.views import (todo_list_create, todo_detail)
+
 urlpatterns = [
-    # path("",)
+    path("todo-create/", todo_list_create),
+    path("todo-detail/<int:id>", todo_detail),
 ]
