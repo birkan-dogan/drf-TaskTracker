1f4d725a46bb666098f883a31e08e3f962ba60fc
birkan <b.561birkan@gmail.com>
2023-01-05 21:15:28 +0300

    using serializers in function-based views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from .models import Todo
+from .serializers import TodoSerializer
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+# function-based view
+
+@api_view(["GET", "POST"])
+def todo_list_create(request):
+
+    if(request.method == "GET"):
+        todos = Todo.objects.filter(is_done = False)
+        serializer = TodoSerializer(todos, many = True)
+        return Response(serializer.data)
+
+    elif(request.method == "POST"):
+        serializer = TodoSerializer(data = request.data)
+
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "DELETE", "PUT"])
+def todo_detail(request, id):
+
+    todo = get_object_or_404(Todo, id = id)
+
+    if(request.method == "GET"):
+        serialize = TodoSerializer(todo)
+        return Response(serialize.data)
+
+    elif(request.method == "PUT"):
+        serializer = TodoSerializer(todo, data = request.data)
+
+        if(serializer.is_valid()):
+            serializer.save()
+            message = {
+                "message": f"{todo.task} is updated"
+            }
+            return Response(message)
+
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    elif(request.method == "DELETE"):
+
+        todo.delete()
+        return Response({"message": f"{todo.task} is deleted"})
+
+
+
